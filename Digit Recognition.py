1532a16d9fb8915bc1844266b8f23a900952c49e
TimKie <55922216+TimKie@users.noreply.github.com>
2020-03-04 15:33:55 +0100

    Digit Recognition code with classification using the "Deep Learning with Python" book

--- Digit Recognition.py
+++ Digit Recognition.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 
+
+# ---------------------------- Define training data -----------------------------------------------------
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+
+# ---------------------------- Define a model (network of layers) ---------------------------------------
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
@@ -17,6 +21,8 @@ predictions
 
 tf.nn.softmax(predictions).numpy()
 
+
+# ---------------------------- Configure the learning process (loss function, optimizer) ----------------
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 loss_fn(y_train[:1], predictions).numpy()
@@ -25,8 +31,12 @@ model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
+
+# ---------------------------- Iterate training data by calling the fit function ------------------------
 model.fit(x_train, y_train, epochs=5)
 
+
+# ---------------------------- Check performance of NN --------------------------------------------------
 model.evaluate(x_test,  y_test, verbose=2)
 
 probability_model = tf.keras.Sequential([

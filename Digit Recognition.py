50af90668b28fc26aa152296c76d548a380321b5
TimKie <55922216+TimKie@users.noreply.github.com>
2020-03-09 16:38:52 +0100

    comments for better understanding added

--- Digit Recognition.py
+++ Digit Recognition.py
@@ -3,43 +3,43 @@ import tensorflow as tf
 
 
 # ---------------------------- Define training data -----------------------------------------------------
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+mnist = tf.keras.datasets.mnist                                         # load data set
+(x_train, y_train), (x_test, y_test) = mnist.load_data()                # load data set
+x_train, x_test = x_train / 255.0, x_test / 255.0                       # covert x_train and x_test to integers
 
 
 # ---------------------------- Define a model (network of layers) ---------------------------------------
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
+model = tf.keras.models.Sequential([                                    # linear stack of layers
+  tf.keras.layers.Flatten(input_shape=(28, 28)),                        # flattens the input (images of digit are 28 by 28 pixels)
+  tf.keras.layers.Dense(128, activation='relu'),                        # fully connected (densely connected) layer (relu is a common activation function)
+  tf.keras.layers.Dropout(0.2),                                         # reduce complexity by deactivating random neurons in a layer
   tf.keras.layers.Dense(10)
 ])
 
-predictions = model(x_train[:1]).numpy()
+predictions = model(x_train[:1]).numpy()                                # produces logits
 predictions
 
-tf.nn.softmax(predictions).numpy()
+tf.nn.softmax(predictions).numpy()                                      # this function (softmax) that takes logits as inputs and converts them to probabilities
 
 
 # ---------------------------- Configure the learning process (loss function, optimizer) ----------------
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)   # function that returns a loss for each example (loss function)
 
-loss_fn(y_train[:1], predictions).numpy()
+loss_fn(y_train[:1], predictions).numpy()                               # use the loss function
 
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+model.compile(optimizer='adam',                                         # configure the model for training, using the adam optimizer (an algorithm for gradient-based optimization of stochastic objective functions)
+              loss=loss_fn,                                             # use the previously defined loss function
+              metrics=['accuracy'])                                     # metric is a function that is used to judge the performance of the model
 
 
 # ---------------------------- Iterate training data by calling the fit function ------------------------
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=5)                                   # adjust the parameters to minimize the loss
 
 
 # ---------------------------- Check performance of NN --------------------------------------------------
-model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(x_test,  y_test, verbose=2)                              # checks the performance of the model (evaluates it)
 
-probability_model = tf.keras.Sequential([
+probability_model = tf.keras.Sequential([                               # function that return the probability of the model
   model,
   tf.keras.layers.Softmax()
 ])
